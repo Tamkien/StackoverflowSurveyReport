771db16e4a596c9863f97cd7c343ffe14f48128c
Tamkien Cao <kiencthe140322@fpt.edu.vn>
2021-06-24 04:35:53 +0700

    add comments

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,12 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
+        //read from csv file
         List<Response> responses =
                 new CsvToBeanBuilder(new FileReader("src\\survey_results_public.csv")).withType(Response.class).build().parse();
+
         for (Response r : responses) {
+            //replace this 'if' with your codes.
             if (r.satisfaction.equals("Neither satisfied nor dissatisfied")) System.out.println(r.languageWorkedWith);
         }
     }
--- src/Response.java
+++ src/Response.java
@@ -1,6 +1,7 @@
 import com.opencsv.bean.CsvBindByPosition;
 
 public class Response {
+    //19 is the index of "JobSat" column. Count from 0, ofc.
     @CsvBindByPosition(position = 19)
     String satisfaction;
     @CsvBindByPosition(position = 22)
