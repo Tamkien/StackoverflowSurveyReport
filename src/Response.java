e360a56b824e48387f609b6d92a4143f30d44591
Tamkien Cao <kiencthe140322@fpt.edu.vn>
2021-07-16 01:07:04 +0700

    add calculations

--- src/Response.java
+++ src/Response.java
@@ -2,9 +2,31 @@ import com.opencsv.bean.CsvBindByPosition;
 
 public class Response {
     //19 is the index of "JobSat" column. Count from 0, ofc.
-    @CsvBindByPosition(position = 19)
-    String satisfaction;
+    @CsvBindByPosition(position = 1)
+    String isCoder;
+    @CsvBindByPosition(position = 7)
+    String salary;
+    @CsvBindByPosition(position = 14)
+    String edLevel;
     @CsvBindByPosition(position = 22)
-    String languageWorkedWith;
+    String languagesWorkedWith;
+    @CsvBindByPosition(position = 42)
+    String orgSize;
+    @CsvBindByPosition(position = 58)
+    String workWeekHrs;
+    @CsvBindByPosition(position = 60)
+    String yearsCodePro;
 
+    @Override
+    public String toString() {
+        return "Response{" +
+                "isCoder='" + isCoder + '\'' +
+                ", salary='" + salary + '\'' +
+                ", edLevel='" + edLevel + '\'' +
+                ", languagesWorkedWith='" + languagesWorkedWith + '\'' +
+                ", orgSize='" + orgSize + '\'' +
+                ", workWeekHrs='" + workWeekHrs + '\'' +
+                ", yearsCodePro='" + yearsCodePro + '\'' +
+                '}';
+    }
 }
