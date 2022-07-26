942ae5b95942d77214d7070f63ffe63c24818867
kienct <kiencthe140322@fpt.edu.vn>
2022-07-27 00:01:28 +0700

    thêm một số thuật toán

--- src/Main.kt
+++ src/Main.kt
@@ -2,13 +2,13 @@ import com.opencsv.bean.CsvToBeanBuilder
 import java.io.FileNotFoundException
 import java.io.FileReader
 import java.util.*
-import kotlin.collections.ArrayList
 
 object Main {
     @Throws(FileNotFoundException::class)
     @JvmStatic
     fun main(args: Array<String>) {
         val data: List<Student?> = readFile()
+//        getBestDisqualified(data)
         val students: List<Student?> = verifyData(data)
         getInput(students)
     }
@@ -28,18 +28,55 @@ object Main {
             getInput(students)
         } else {
             student.toString()
-            doSortByB00(student, students)
+            doSort(student, students, "B00")
+            tinhPhoDiem(students)
         }
     }
 
-    private fun doSortByB00(student: Student, students: List<Student?>) {
-        val sortedStudents = students.sortedByDescending { (it!!.toan ?: 0.0) + (it.hoa ?: 0.0) + (it.sinh ?: 0.0) }
+    private fun tinhPhoDiem(students: List<Student?>) {
+        for (i in 0..599) {
+            val thisPoints = mutableListOf<Student>()
+            for (s in students) {
+                if (s hasBetterB00MarkThan i
+//                    && s!!.hasBestMarkInB00()
+                ) thisPoints.add(s!!)
+            }
+            println("> ${i * 1.0 / 20} ${thisPoints.size} ")
+        }
+    }
+
+    private infix fun Student?.hasBetterB00MarkThan(i: Int): Boolean {
+        return this!!.getB00Mark() > i / 20.0
+    }
+
+    private fun Student.hasBestMarkInB00(): Boolean {
+        return this.getB00Mark() > this.getA01Mark()
+                && this.getB00Mark() > this.getA00Mark()
+                && this.getB00Mark() > this.getA02Mark()
+    }
+
+    private fun doSort(student: Student, students: List<Student?>, s: String) {
+        val sortedStudents = students.sortedByDescending {
+            when (s) {
+                "A00" -> it!!.getA00Mark()
+                "A01" -> it!!.getA01Mark()
+                "D01" -> it!!.getD01Mark()
+                "A02" -> it!!.getA02Mark()
+                else -> it!!.getB00Mark()
+            }
+        }
         val thisStudent = sortedStudents.firstOrNull { it!!.id == student.id }
         if (thisStudent != null) println("Thứ hạng: ${sortedStudents.indexOf(thisStudent)}")
     }
 
     private fun verifyData(students: List<Student?>): List<Student?> {
-        return students.filter { it?.id?.div(1000000) == 1 || it?.id?.div(1000000) in (5..30) || it?.id?.div(1000000) == 3 }
+        return students.filter {
+            (it?.id?.div(1000000) == 1
+                    || it?.id?.div(1000000) in (5..28)
+                    || it?.id?.div(1000000) == 3)
+                    && it!!.hasBestMarkInB00()
+                    && it!!.isQualified()
+        }
     }
 
     private fun readFile(): List<Student?> {
@@ -48,4 +85,46 @@ object Main {
         ).build().parse() as List<Student?>
     }
 
+}
+
+private fun getBestDisqualified(data: List<Student?>) {
+    var bestDisqualified = Student()
+    data.forEach {
+        if (it!!.getB00Mark() >= bestDisqualified.getB00Mark() && !it.isQualified())
+            bestDisqualified = it
+    }
+    println(bestDisqualified.toString())
+}
+
+private fun Student.isQualified(): Boolean {
+    if (this.toan != 0.01 && this.toan <= 1.0) return false
+    if (this.van != 0.01 && this.van <= 1.0) return false
+    if (this.anh != 0.01 && this.anh <= 1.0) return false
+    if (this.ly != 0.01 && this.ly <= 1.0) return false
+    if (this.hoa != 0.01 && this.hoa <= 1.0) return false
+    if (this.sinh != 0.01 && this.sinh <= 1.0) return false
+    if (this.su != 0.01 && this.su <= 1.0) return false
+    if (this.dia != 0.01 && this.dia <= 1.0) return false
+    if (this.gdcd != 0.01 && this.gdcd <= 1.0) return false
+    return true
+}
+
+private fun Student.getB00Mark(): Double {
+    return this.toan + this.hoa + this.sinh
+}
+
+private fun Student.getA01Mark(): Double {
+    return this.toan + this.hoa + this.ly
+}
+
+private fun Student.getA00Mark(): Double {
+    return this.toan + this.ly + this.anh
+}
+
+private fun Student.getA02Mark(): Double {
+    return this.toan + this.hoa + this.anh
+}
+
+private fun Student.getD01Mark(): Double {
+    return this.toan + this.van + this.anh
 }
--- src/Student.java
+++ src/Student.java
@@ -4,23 +4,23 @@ public class Student {
     @CsvBindByPosition(position = 0)
     int id = 0;
     @CsvBindByPosition(position = 1)
-    Double toan = 0.0;
+    Double toan = 0.01;
     @CsvBindByPosition(position = 2)
-    Double van = 0.0;
+    Double van = 0.01;
     @CsvBindByPosition(position = 3)
-    Double anh = 0.0;
+    Double anh = 0.01;
     @CsvBindByPosition(position = 4)
-    Double ly = 0.0;
+    Double ly = 0.01;
     @CsvBindByPosition(position = 5)
-    Double hoa = 0.0;
+    Double hoa = 0.01;
     @CsvBindByPosition(position = 6)
-    Double sinh = 0.0;
+    Double sinh = 0.01;
     @CsvBindByPosition(position = 7)
-    Double su = 0.0;
+    Double su = 0.01;
     @CsvBindByPosition(position = 8)
-    Double dia = 0.0;
+    Double dia = 0.01;
     @CsvBindByPosition(position = 9)
-    Double gdcd = 0.0;
+    Double gdcd = 0.01;
 
     @Override
     public String toString() {
@@ -37,4 +37,5 @@ public class Student {
                 ", gdcd=" + gdcd +
                 '}';
     }
+
 }
